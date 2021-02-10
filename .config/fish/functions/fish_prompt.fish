a5b1bc97fbaaa22a3bdea584f151cff48af430ff
Carlos Troncoso Pizarro <carlostp7@uc.cl>
2021-02-09 23:21:08 -0300

    anadidos los aliases y comandos para fish

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,20 @@
+# aliases
+
+alias ls "exa --group-directories-first"
+alias lsa "exa -la --group-directories-first"
+alias tree "exa -T"
+alias cat "ccat"
+alias gitm "git commit -m"
+alias upt "sudo pacman -Sy && sudo pacman -Syu && yay -Sy && yay - Syu"
+
+
+
+# funcion para sudo !!
+# https://unix.stackexchange.com/questions/235704/fish-sudo-command-not-found
+function sudo --description "funcion para sudo !!"
+    if test "$argv" = !!
+    eval command sudo $history[1]
+else
+    command sudo $argv
+    end
+end
