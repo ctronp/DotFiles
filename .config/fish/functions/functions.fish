63574675b3079eca4d71927262ac1376eab36c25
Carlos Troncoso Pizarro <carlostp7@uc.cl>
2021-03-14 00:04:34 -0300

    anadidos comandos para LaTeX y mejorada la identacion

--- .config/fish/functions/functions.fish
+++ .config/fish/functions/functions.fish
@@ -5,6 +5,7 @@ alias lsa "exa -la --group-directories-first"
 alias tree "exa -T"
 alias cat "ccat"
 alias gitm "git commit -m"
+alias texrtm "latexmk -pdf -pvc"
 
 # script para actualizar todo
 alias uptall "$HOME/bin/updateall"
@@ -13,8 +14,18 @@ alias uptall "$HOME/bin/updateall"
 # https://unix.stackexchange.com/questions/235704/fish-sudo-command-not-found
 function sudo --description "funcion para sudo !!"
     if test "$argv" = !!
-    eval command sudo $history[1]
-else
-    command sudo $argv
+        eval command sudo $history[1]
+    else
+        command sudo $argv
+    end
+end
+
+# funcion para crear pdf desde latex
+function texpdf --description "funcion para crear pdf desde .tex sin archivos indeseados"
+    if test (count $argv) = 1
+        command rubber --pdf $argv
+        command rubber --clean $argv
+    else
+        command echo "necesitas introducir el nombre del archivo"
     end
 end
